ab7cdee4d30b3be39803d6fb42a03e40179cdd2c
jamesdykes1 <up784157@myport.ac.uk>
2017-12-11 19:47:36 +0000

    colour contrast edits

--- public/js/colorblind.js
+++ public/js/colorblind.js
@@ -1,31 +1,7 @@
-function changeColor(){
+function changeColor() {
+  document.getElementById('originalStyle').href = 'css/cbstyle.css';
+}
 
-    var cssId = 'myCss';
-    // var orgId = 'originalStyle';
-    var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    if (!document.getElementById(cssId))
-    {
-        link.id   = cssId;
-        link.rel  = 'stylesheet';
-        link.type = 'text/css';
-        link.href = 'css/cbstyle.css';
-        link.media = 'all';
-        head.appendChild(link);
-        console.log(head.childNodes[1]);
-
-    }
-
-    // else {
-    //
-    //     head.removeChild(head.childNodes[1]);
-    //     link.id   = orgId;
-    //     link.rel  = 'stylesheet';
-    //     link.type = 'text/css';
-    //     link.href = 'css/style.css';
-    //     link.media = 'all';
-    //     head.appendChild(link);
-    //     console.log(head.childNodes[1]);
-    //
-    // }
+function revertColor() {
+  document.getElementById('originalStyle').href = 'css/style.css';
 }
